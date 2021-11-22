64b5a0bf869cd1b420bc29d55093fe203302eebb
Cedric Vidonne <cedric@vidonne.me>
2021-11-22 11:35:04 +0100

    feat: added img_to_dark attribute
    Added a darken image attribute for the cover image of paged_report to make the text easier to read depending on the darknes of the image fix #6

--- R/paged_report.R
+++ R/paged_report.R
@@ -1,6 +1,7 @@
 #' Function for UNHCR paged report template
 #'
 #' @param front_img Cover image
+#' @param img_to_dark Darken cover image
 #' @param other_css Add extra css
 #' @param toc Table of content
 #' @param toc_depth Table of content depth
@@ -9,6 +10,7 @@
 #' @return A pagedown report
 #' @export
 paged_report <- function(front_img = NULL,
+                         img_to_dark = FALSE,
                          other_css = NULL,
                          toc = TRUE,
                          toc_depth = 2,
@@ -26,6 +28,21 @@ paged_report <- function(front_img = NULL,
     front_img <-
       pkg_resource("img/cover-example.jpg")
   }
+
+  # darken img
+  # idea from https://github.com/rfortherestofus/pagedreport
+  if (img_to_dark == TRUE) {
+    # opacity
+    front_img_init <-
+      magick::image_read(front_img)
+    front_img_ok <-
+      magick::image_colorize(front_img_init, opacity = 50, color = "black")
+
+    # path to image
+    front_img <- paste0(tempfile("front_img"), ".jpg")
+    magick::image_write(front_img_ok, front_img, format = "jpg")
+  }
+
   # template
   pagedown::html_paged(
     css = c(base_css, main_css, other_css),
