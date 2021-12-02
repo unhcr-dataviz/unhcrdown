0cc70b1f90500825e7dfc2da53ed4550ecdfe2a3
Cedric Vidonne <cedric@vidonne.me>
2021-12-02 17:42:32 +0100

    feat: Revised xaringan and CSS
    Changed the xaringan fucntion and css to fit with UNHCR style and the package structure. Start work on compartimenting CSS for different doc use.

---
+++ R/html_slides.R
@@ -0,0 +1,33 @@
+#' Convert to UNHCR branded HTML presentation
+#'
+#' Format for converting from R Markdown to an UNHCR branded HTML presentation
+#'
+#' @rdname html_slides
+#'
+#' @importFrom xaringan moon_reader
+#'
+#' @param other_css Add extra css
+#' @param nature See ?xaringan::moon_reader
+#' @param ... extra parameters to pass to `xaringan::moon_reader`
+#'
+#' @export
+html_slides <- function(other_css = NULL,
+                        nature =  list(countIncrementalSlides = "no",
+                                       highlightLines = "yes",
+                                       highlightStyle = "github",
+                                       ratio = 16:9),
+                        ...) {
+  # css files
+  color_variables_css <- pkg_resource("css/color_variables.css")
+  fonts_css <- pkg_resource("css/fonts.css")
+  html_slides_css <- pkg_resource("css/html_slides.css")
+
+  # html footer
+  footer_html <- pkg_resource("html/footer_html_slides.html")
+
+  xaringan::moon_reader(
+    css = c(color_variables_css, fonts_css , html_slides_css, other_css),
+    includes = list(after_body = footer_html),
+    ...
+  )
+}
