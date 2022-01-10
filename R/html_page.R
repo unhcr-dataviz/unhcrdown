95a1350341966453e5ef688d7884f2aac82ff608
Cedric Vidonne <cedric@vidonne.me>
2022-01-10 16:43:48 +0100

    fix: bslib cache issue

--- R/html_page.R
+++ R/html_page.R
@@ -2,13 +2,14 @@
 #'
 #' Format for converting from R Markdown to an UNHCR branded HTML website
 #'
+#' @importFrom unhcrdesign use_unhcr_bslib
 #' @rdname html_page
 #'
 #' @param ... extra parameters to pass to `rmarkdown::html_document`
 #'
 #' @export
 html_page <- function(...) {
-  unhcr_theme <- unhcrdesign::unhcr_bslib
+  unhcr_theme <- unhcrdesign::use_unhcr_bslib()
   rmarkdown::html_document(
     theme = unhcr_theme,
     ...)
