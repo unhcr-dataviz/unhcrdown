c607ef51722474a909daa57e129691eb23e723e9
Cedric Vidonne <cedric@vidonne.me>
2023-12-29 13:36:16 +0100

    add basic scss structure and move resources back to package

---
+++ data-raw/scss.r
@@ -0,0 +1,21 @@
+# Generate CSS from SCSS
+
+compile_css <- function(
+    scss_file = "",
+    css_file = "",
+    output_style = "compressed") {
+  sass::sass(
+    sass::sass_file(
+      here::here("data-raw", "scss", scss_file)
+    ),
+    output = here::here("inst", "resources", "css", css_file),
+    options = sass::sass_options(output_style = output_style)
+  )
+}
+
+# Generate paged_report.css
+
+compile_css(
+  scss_file = "paged_report.scss",
+  css_file = "paged_report.css"
+)
